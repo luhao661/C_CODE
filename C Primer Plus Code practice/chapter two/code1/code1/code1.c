88b62448b0fc31e2e74f60e29d42ee73f388f949
luhao <2398186000@qq.com>
2022-04-07 11:32:47 +0800

    第2章

---
+++ C Primer Plus Code practice/chapter two/code1/code1/code1.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a = 1;
+	printf("I am a smart computer\n");
+	printf("my favorite number is %d becasuse it is the first.\n", a );
+	return 0;
+}
