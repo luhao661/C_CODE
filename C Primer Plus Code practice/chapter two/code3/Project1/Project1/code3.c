cd12a90ab5a68d057bc59f8bd33fe697a5e3b735
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-04-09 08:33:49 +0800

    Add files via upload

---
+++ C Primer Plus Code practice/chapter two/code3/Project1/Project1/code3.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//void diaoyong(void);
+//int main(void)
+//{
+//	printf("我将调用一个函数\n");
+//	diaoyong();
+//	printf("调用成功\n");
+//
+//}
+//void diaoyong(void)
+//{
+//	printf("哈哈\n");
+//}
+
+int main(void)
+{
+	int n, n2, n3;
+	n = 5;
+	n2 = n * n;
+	n3 = n2 * n2;
+	/**/
+	printf("%d %d %d", n, n2, n3);
+	return 0;
+}
