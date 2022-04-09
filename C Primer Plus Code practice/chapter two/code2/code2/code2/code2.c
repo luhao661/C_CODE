08d9c8d9e3d7fbd22cd99cc278d6a96594a6a0d6
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-04-09 08:35:35 +0800

    Add files via upload

---
+++ C Primer Plus Code practice/chapter two/code2/code2/code2/code2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//
+//
+//	int cat;
+//	cat = 1;
+//	int dog;
+//	dog = 2;
+//}
+
+int main(void)
+{
+	int a, b;
+
+	a = 2;
+	b = a * 6;
+	printf("There are %d and %d",a,b);
+	return 0;
+}
