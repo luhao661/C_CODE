6546cb80ffda2eabf00485a02164e17e7837d1db
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-04-23 21:45:49 +0800

    Add files via upload

--- C Primer Plus Code practice/chapter 6/code3/Project1/Project1/code3.c
+++ C Primer Plus Code practice/chapter 6/code3/Project1/Project1/code3.c
@@ -1,32 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//计算数的整数次幂，并返回计算结果
-double power(double n, int p);
+//计算数的整数次幂，并返回计算结果（使用函数返回值的循环）
+//double power(double n, int p);
+//int main(void)
+//{
+//	double x;
+//	int exp;
+//	
+//	printf("请输入一个数字和该数字的整数次幂数：\n");
+//	printf("（输入q以退出程序）                 ");
+//	while (scanf("%lf%d", &x,& exp) == 2)
+//	{
+//		printf("%f的%d次方是%f",x,exp, power(x, exp));
+//		printf("输入下一对数字：");
+//	}
+//	printf("Done!");
+//
+//	return 0;
+//}
+//double power(double n, int p)
+//{
+//	double pow = 1;
+//	int i;
+//
+//	for (i = 1; i <= p; i++)
+//		pow *= n;
+//
+//	return pow;
+//}
+
+
+//复习题6.
+#define ROWS 4
+#define COLUMNS 8
 int main(void)
 {
-	double x;
-	int exp;
-	double xpow;
-
-	printf("请输入一个数字和该数字的整数次幂数：\n");
-	printf("（输入q以退出程序）                 ");
-	while (scanf("%lf%d", &x,& exp) == 2)
+	int i,j;
+	for (i = 1; i <= ROWS; i++)
 	{
-		xpow = power(x, exp);
-		printf("%f的%d次方是%f",x,exp,xpow);
-		printf("输入下一对数字：");
+		for (j = 1; j <= COLUMNS; j++)
+			printf("$");
+		printf("\n");
 	}
-	printf("Done!");
 
 	return 0;
-}
-double power(double n, int p)
-{
-	double pow = 1;
-	int i;
-
-	for (i = 1; i <= p; i++)
-		pow *= n;
-
-	return pow;
 }
