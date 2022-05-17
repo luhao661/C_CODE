874368aa14810f9b4f68ad1e4b4d2c46cbd549aa
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-05-17 20:07:10 +0800

    Add files via upload

--- C Primer Plus Code practice/chapter 10/code1/Project1/Project1/code1.c
+++ C Primer Plus Code practice/chapter 10/code1/Project1/Project1/code1.c
@@ -201,7 +201,7 @@ int sum(int* ar, int n)
 
 
 //使用指针计算数组中的元素的和(法二)
-#if 1
+#if 0
 #define SIZE 10
 int sum(int* start, int *end);
 int main(void)
@@ -228,5 +228,126 @@ int sum(int* start, int*end)
 		sum += *start;
 
 	return sum;
+}
+#endif
+
+
+//指针运算中的优先级
+#if 0
+int data[2] = { 100,200 };
+int moredata[2] = { 300,400 };
+int main(void)
+{
+	int* p1, * p2, * p3;
+
+	p1 = p2 = data;
+	p3 = moredata;
+
+	printf("*p1=%d,         *p2=%d,         *p3=%d\n",*p1,*p2,*p3);
+	printf("*p1++=%d,       *++p2=%d,      (*p3)++=%d\n", *p1++, *++p2, ( * p3)++);
+	printf("*p1=%d,         *p2=%d,         *p3=%d\n", *p1, *p2, *p3);
+
+	return 0;
+}
+#endif
+
+
+//指针操作
+#if 0
+int main(void)
+{
+	int shuzu[5] = { 100,200,300,400,500 };
+	int* p1, * p2, * p3;
+
+	p1 = shuzu;
+	p2 = &shuzu[2];
+
+	printf("指针指向的地址			指针指向地址中的值			指针本身的地址\n");
+	printf("p1=%p		*p1=%d					&p1=%p\n",p1,*p1,&p1	);
+
+	//指针加法
+	p3 = p1 + 4;
+	printf("p1+4=%p		*(p1+4)=%d\n", p1+4, *(p1+4));
+	p1++;
+	printf("p1++后\n");
+	printf("p1=%p		*p1=%d			  	        &p1=%p\n", p1, *p1,&p1);
+
+	p2--;
+	printf("p2减1后\n");
+	printf("p2=%p		*p2=%d			  	        &p2=%p\n", p2, *p2, &p2);
+
+	--p1;
+	++p2;
+	printf("p1,p2还原\n");
+
+	printf("p1=%p,p2=%p\n",p1,p2);
+
+	//指针相减
+	printf("p1=%p,p2=%p,p2-p1=%td\n", p1, p2,p2-p1);
+	//指针减去一个整数
+	printf("p3=%p,p3-2=%p\n",p3,p3-2);
+
+	//指针间的赋值
+	//p1 = p1 + p2;无效
+
+	return 0;
+}
+#endif
+
+
+//处理数组的函数
+#if 0
+#define SIZE 5
+void show_array(const double* ar, int n);
+void mult_array(double ar[], int n, double mult);
+int main(void)
+{
+	double dip[SIZE] = {20.0,17.66,8.2,15.3,22.22};
+
+	printf("原始的数组中的数据是：\n");
+	show_array(dip,SIZE);
+	printf("处理后数组中的数据是：\n");
+	mult_array(dip,SIZE,2.5);
+	show_array(dip,SIZE);
+
+	return 0;
+}
+void show_array(const double* ar, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%8.3f",*(ar+i));
+
+	putchar('\n');
+}
+void mult_array(double ar[], int n, double mult)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		ar[i] *= mult;
+	//*(ar + i) *= mult;
+}
+#endif
+
+
+//理解指针如何表示二维数组
+#if 1
+int main(void)
+{
+	int zippo[4][2] = { {2,4},{6,8},{1,3},{5,7} };
+
+	printf("zippo=%p, zippo+1=%p\n",zippo,zippo+1);
+	printf("zippo[0]=%p, zippo[0]+1=%p\n",zippo[0],zippo[0]+1);
+	printf("*zippo=%p, *zippo+1=%p\n\n",*zippo,*zippo+1);
+
+	printf("zippo[0][0]=%d\n",zippo[0][0]);
+	printf("*zippo[0]=%d\n",*zippo[0]);
+	printf("**zippo=%d\n\n", **zippo);
+
+	printf("zippo[2][1]=%d\n",zippo[2][1]);
+	printf("*(*(zippo+2)+1)=%d\n\n",*(*(zippo+2)+1));
+
+
+	return 0;
 }
 #endif
