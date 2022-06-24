fe8561c754b8ddc6040e82a772082ac465c388bf
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-06-24 20:17:54 +0800

    Add files via upload

--- C Primer Plus Code practice/chapter 14/code1/Project1/Project1/code1.c
+++ C Primer Plus Code practice/chapter 14/code1/Project1/Project1/code1.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>//malloc(), free()
 //
 #if 1
 #endif
@@ -388,7 +389,7 @@ char* s_gets(char* string, int n)
 
 
 //使用结构参数和返回值进行函数间双向通信
-#if 1
+#if 0
 #define LEN 30
 struct names					//声明一个标记为names的结构
 {
@@ -449,4 +450,313 @@ char* s_gets(char* string, int n)
 	}
 	return fanhui;
 }
+#endif
+
+
+//使用指向char的指针来代替字符数组
+#if 0
+#define LEN 30
+struct names					//声明一个标记为names的结构
+{
+	char *first;//使用指针来代替字符数组
+	char *last;
+	int letters;
+};
+
+void getinfo(struct names*);//声明一个函数，形参是一个指向names结构布局的结构指针
+void makeinfo(struct names*);
+void showinfo(const struct names*);
+void cleanup(struct names*);
+char* s_gets(char* string, int n);
+
+int main(void)
+{
+	struct names person;//声明一个使用names结构布局的结构变量person
+
+	getinfo(&person);//对结构变量person的成员赋值
+	makeinfo(&person);
+	showinfo(&person);
+	cleanup(&person);
+
+	return 0;
+}
+void getinfo(struct names* p)
+{
+	char temp[LEN];
+	puts("请输入你的姓");
+	s_gets(temp, LEN);//数据暂存入temp数组
+	p->first = (char*)malloc(strlen(temp)+1);//first指针指向分配到的内存，这样就不会造成s_gets(p->first, LEN);
+	strcpy(p->first,temp);																			//导致数据被存在任何可能造成程序崩溃的地方。
+
+	puts("请输入你的名");
+	s_gets(temp, LEN);
+	p->last = (char*)malloc(strlen(temp) + 1);
+	strcpy(p->last, temp);
+}
+void makeinfo(struct names* p)
+{
+	p->letters = strlen(p->first) + strlen(p->last);
+}
+void showinfo(const struct names* p)
+{
+	printf("%s %s,你的名字占%d个字节\n", p->first, p->last, p->letters);
+}
+void cleanup(struct names*p)
+{
+	free(p->first);
+	free(p->last);
+}
+char* s_gets(char* string, int n)
+{
+	char* fanhui;
+	char* find;
+
+	fanhui = fgets(string, n, stdin);
+	if (fanhui)
+	{
+		find = strchr(string, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+	return fanhui;
+}
+#endif
+
+
+//使用代表结构内容的复合字面量
+#if 0
+#define MAX_SHUMING 41 //最大书名长度
+#define MAX_ZUOZHE 41 //最大作者姓名长度
+
+char* s_gets(char* string, int n);
+
+struct book											//结构声明（也叫创建一个结构模板）	//book是个结构的标记，后面程序可以使用book引用此结构
+{															//声明一个标记为book的结构
+	char shuming[MAX_SHUMING];
+	char zuozhe[MAX_ZUOZHE];
+	float value;
+};
+
+int main(void)
+{
+	struct book library;						//声明一个使用book结构布局的结构变量library（把library声明为一个book类型的变量）
+
+	puts("请输入考试分数：");
+	int score;
+
+	scanf("%d",&score);
+
+	if (score >= 84)
+		library = (struct book){
+	"shuming1",
+	"zuozhe1",
+	11.25
+	};
+	else 
+		library = (struct book){
+"shuming2",
+"zuozhe2",
+5.99
+	};
+
+	printf("你应该读的书是%s:《%s》（￥%.2f）", library.zuozhe, library.shuming, library.value);
+
+	return 0;
+}
+#endif
+
+
+//使用伸缩型数组成员
+#if 0
+struct flex
+{
+	size_t count;
+	double average;
+	double scores[];
+};
+void showflex(const struct flex *p);//形参：一个指向flex结构布局的结构的指针p
+
+int main(void)
+{
+	struct flex* p1;
+	int n = 5;
+	int i;
+	int total = 0;
+
+	p1 = malloc(sizeof(struct flex)+n*sizeof(double));//为常规内容和伸缩型数组成员分配所需的内存空间
+
+	p1->count = n;
+	for (i = 0; i < n; i++)
+	{
+		p1->scores[i] = 20.0 - i;//为5个数组元素赋值
+		total += p1->scores[i];
+	}
+	p1->average = total / n;
+
+	showflex(p1);
+
+	free(p1);
+
+	return 0;
+}
+void showflex(const struct flex* p)
+{
+	printf("scores:");
+	for (int i = 0; i < p->count; i++)
+		printf("%-8.2f",p->scores[i]);
+
+	printf("\naverage:%.2f\n",p->average);
+}
+#endif
+
+
+//使用结构数组进行函数间双向通信（本质上还是把结构的地址作为实参传递）
+#if 0
+#define FUNDLEN 50
+
+struct funds//	//声明一个标记为funds的结构
+{
+	char bank[FUNDLEN];
+	double bankfund;
+	char save[FUNDLEN];
+	double savefund;
+};
+double sum(const struct funds *money,int n);//声明一个函数，形参是一个使用funds结构布局的结构变量money
+
+int main(void)
+{
+	struct funds zhangsan[2] =//声明一个使用funds结构布局的结构变量zhangsan，并对其初始化
+	{
+		{
+		"Bank of China",
+		4032.27,
+		"Luck's Savings",
+		8543.94
+		},
+		{
+		"China Construction Bank",
+		3620.88,
+		"Party Time Savings",
+		3802.91
+	    }
+
+	};
+	printf("张三总共有%.2f块钱\n", sum(zhangsan,2));//************拷贝给函数zhangsan这个结构   ╳    *******************
+																					  //正确理解：传递的是zhangsan数组的地址，即该数组首元素的地址&zhangsan[0]
+	return 0;																	  //对比第320行。结构变量名不是结构的地址。但此处是zhangsan是个结构数组，
+}																					  //*********因此zhangsan是数组地址也是结构地址**********
+double sum(const struct funds *money,int n)
+{												//money指向zhangsan[0]，存储&zhangsan[0]
+	double total;
+	int i;
+	for (i = 0, total = 0; i < n; i++)
+	{
+		total += money[i].bankfund + (   * (money + i)   ).savefund;//money[i]相当于*(money+i)
+	}																									    //money[0]就是zhangsan[0]结构变量
+	return total;																				    //money[1]就是zhangsan[1]结构变量
+}
+#endif
+
+
+//在文件中保存结构中的内容
+#if 1
+#define MAX_SHUMING 41 //最大书名长度
+#define MAX_ZUOZHE 41 //最大作者姓名长度
+#define MAX_SHULIANG 100//最大书籍数量
+
+char* s_gets(char* string, int n);
+
+struct book												//声明一个标记为book的结构
+{
+	char shuming[MAX_SHUMING];
+	char zuozhe[MAX_ZUOZHE];
+	float value;
+};
+
+int main(void)
+{
+	struct book library[MAX_SHULIANG];//声明一个使用book结构布局的结构变量数组library[100]
+	int count = 0;
+
+	//打开文件，并计算文件中原有的图书数量
+	FILE* fp;
+	if ((fp = fopen("book_data.erjinzhi", "a+b")) == NULL)
+	{
+		fputs("打开文件失败！",stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	rewind(fp);											//待读取文件数据拷贝进内存中的地址，待读数据块的大小，待读数据块数量，待读取的文件
+	while (count < MAX_SHULIANG && fread(&library[count], sizeof(struct book), 1, fp)==1)
+	{
+		if (count == 0)
+			puts("当前文件中含有的图书信息如下：");
+
+		printf("%s:《%s》（￥%.2f）\n", library[count].zuozhe, library[count].shuming, library[count].value);
+		count++;
+	}
+
+	//保存文件中图书数量的值
+	int filecount = count;
+
+	//判断文件中图书数量是否大于了100本
+	if (filecount == MAX_SHULIANG)
+	{
+		fprintf(stderr,"book_data.txt已满！");
+		exit(EXIT_FAILURE);
+	}
+
+	//文件中图书数量没到100本
+	printf("请输入书名(在新行输入^Z或在新行输入[Enter]以结束)：\n");
+	while (count < MAX_SHULIANG && s_gets(library[count].shuming, MAX_SHUMING) != NULL && library[count].shuming[0] != '\0')
+	{
+		puts("请输入作者姓名：");
+		s_gets(library[count].zuozhe, MAX_ZUOZHE);
+
+		puts("请输入售价：");
+		scanf("%f", &library[count].value);
+		while (getchar() != '\n')//消除scanf()执行后缓冲区留下的\n
+			continue;
+
+		count++;
+
+		printf("请输入书名(在新行输入^Z或在新行输入[Enter]以结束)：\n");
+	}
+
+	//显示添加的图书，并把新加的图书也保存在文件中
+	printf("以下是图书目录：\n");
+	for (int index = 0; index < count; index++)
+	{
+		printf("%s:《%s》（￥%.2f）\n", library[index].zuozhe, library[index].shuming, library[index].value);
+	}
+	//待写入的文件数据所在的内存中的地址，待写数据块的大小，待写数据块数量，待写入的文件
+	fwrite(&library[count], sizeof(struct book), count-filecount, fp);
+																		  //count-filecount得出新添加的图书数量
+	puts("存入文件已完成！");
+	fclose("fp");
+
+	return 0;
+}
+char* s_gets(char* string, int n)
+{
+	char* fanhui;
+	char* find;
+
+	fanhui = fgets(string, n, stdin);
+	if (fanhui)
+	{
+		find = strchr(string, '\n');
+		if (find)
+			*find = '\0';
+		else
+			while (getchar() != '\n')
+				continue;
+	}
+	return fanhui;
+}
+//命令行运行程序：D:\CODE\C_CODE\C Primer Plus Code practice\chapter 14\code1\Project1\x64\Debug
+//Project1.exe
 #endif
