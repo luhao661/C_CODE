0b9de36dc4b37a390e48a9e0308227aaa0339a5a
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-07-01 19:14:34 +0800

    Add files via upload

--- C Primer Plus Code practice/chapter 14/code2/Project2/Project2/code2.c
+++ C Primer Plus Code practice/chapter 14/code2/Project2/Project2/code2.c
@@ -14,7 +14,7 @@
 typedef struct //月份					//声明一个省略了标记的结构，并用YUEFEN类型名来命名该结构类型
 {
 	char yuefen_min[10];
-	char min_suoxie[4];
+	char min_suoxie[4];//三个字母加一个空字符
 	int yuefen_tianshu;
 	int yuefen_hao;//月份号
 } YUEFEN;
@@ -27,12 +27,7 @@ YUEFEN yinian[12] =			  //声明一个使用YUEFEN结构布局的结构数组yinian[12]
 		31,	////七月写为July，可缩写为Jul。八月写为August，可缩写为Aug。九月写为September，可缩写为Sept。十月写为October，可缩写为Oct。十一月写为November，可缩写为Nov。12月写为December，可缩写为Dec。
 		1			
 	},
-	{
-		"February",
-		"Feb",
-		29,
-		2
-	},
+	{"February",	"Feb", 29, 2	},
 	{
 		"March",
 		"Mar",
@@ -111,7 +106,7 @@ int jisuantianshu(YUEFEN * yuefen, int yuefenhao)
 {
 	int sum = 0;
 
-	for (int i = 0; i < yuefenhao - 1; i++)
+	for (int i = 0; i < yuefenhao - 1; i++)//索引值i为0 时，操作的是1月份
 	{
 		sum += (yuefen+i)->yuefen_tianshu;
 	}
@@ -145,7 +140,7 @@ int main(void)
 		},
 	};
 #endif
-
+													//******************************注**************************************
 	//lens_shuzu[2] =					//这样不行，P243：除初始化外，不允许使用花括号的形式给数组赋值
 	//{
 	//500,
@@ -157,11 +152,31 @@ int main(void)
 #if 1
 	LENS lens_shuzu[10] =
 	{			
-		lens_shuzu[2].foclen=500,
+		[2].foclen=500,
 			2.0,
 			"Remarkata"		
 	};
 #endif
+
+	return 0;
+}
+#endif
+//6.a.改正（题目要求是单独给成员赋值而不是初始化）
+#if 0
+typedef struct lens
+{
+	float foclen;
+	float fstop;
+	char brand[30];
+}LENS;
+
+int main(void)
+{
+	LENS lens_shuzu[10];
+	lens_shuzu[2].foclen = 500;
+	lens_shuzu[2].fstop = 2.0;
+	//lens_shuzu[2].brand = {"Remarkata"};//无效
+	strcpy(lens_shuzu[2].brand, "Remarkata");
 
 	return 0;
 }
@@ -242,6 +257,7 @@ int main(void)
 	return 0;
 }
 #endif
+//或：strlen(willie.name.fname)+strlen(willie.name.lname);
 
 
 //9.
@@ -266,30 +282,106 @@ struct gas
 	float mpg;
 };
 
-void jisuan_mpg1(struct gas* distance, struct gas* gals, struct gas* mpg);
 float jisuan_mpg2(struct gas *distance, struct gas* gals);
+void jisuan_mpg1(struct gas* distance, struct gas* gals, struct gas* mpg);
 
-
-void jisuan_mpg1(struct gas* distance, struct gas* gals, struct gas* mpg)
-{
-	*mpg = *distance / (*gals);
-}
 float jisuan_mpg2(struct gas* distance, struct gas* gals)
 {
 	float mpg;
 	mpg = ( * distance )/ (*gals);
 	return mpg;
 }
+void jisuan_mpg1(struct gas* distance, struct gas* gals, struct gas* mpg)
+{
+	*mpg = *distance / (*gals);
+}
 #endif
+//法二：a.方法：传入结构   b.方法：传入结构指针
 
 
 //11.
-#if 1
+#if 0
 enum choices  {no,yes,maybe};   //中间不用'='号
 #endif
 
 
 //12.
-#if 1
+#if 0
 char* (*function_p)(char *p,char ch);
+#endif
+
+
+//13.
+#if 0
+//double(       (*function_p)   [4]     )(double a,double b);																		//报错：不允许使用函数数组
+double(*function_p1)(double a, double b);
+double(*function_p2)(double a, double b);
+double(*function_p3)(double a, double b);
+double(*function_p4)(double a, double b);
+
+double(*function_p[4])(double a, double b);//声明4个函数指针
+
+int main (void)
+{
+	function_p[0] = function_p1;
+	function_p[1] = function_p2;
+	function_p[2] = function_p3;
+	function_p[3] = function_p4;
+	
+	function_p[1](10.0,2.5);							//法一
+	(*function_p[1])(10.0, 2.5);						//法二
+
+	//*(function_p + 1)(10.0,2.5);//错误
+	(* (function_p + 1))(10.0, 2.5);				//法二的另一种写法
+}
+double(*function_p1)(double a, double b);
+double(*function_p2)(double a, double b)
+{
+	printf("%.2lf",a+b);
+}
+double(*function_p3)(double a, double b);
+double(*function_p4)(double a, double b);
+#endif
+//***********************************************注************************************************************************************
+//错误：在没有函数名时就用函数指针的形式声明函数
+
+#if 1
+//double(       (*function_p)   [4]     )(double a,double b);																		//报错：不允许使用函数数组
+double function1 (double a, double b);
+double function2 (double a, double b);
+double function3 (double a, double b);
+double function4 (double a, double b);
+
+double(*function_p[4])(double a, double b);//声明1个函数指针数组，其中的每个元素都是指向函数的指针
+
+//法二：直接初始化函数指针数组
+//double(*function_p[4])(double a, double b) = { function_p1,function_p2 ,function_p3,function_p4 };
+//法三：用typedef+初始化函数指针数组
+//typedef double(*ptype)(double a, double b);
+//ptype function_p[4]= { function_p1,function_p2 ,function_p3,function_p4 };
+
+int main(void)
+{
+//法一：在主函数中对元素赋值
+	function_p[0] = function1;
+	function_p[1] = function2;
+	function_p[2] = function3;
+	function_p[3] = function4;
+
+	function_p[1](10.0, 2.5);							//调用函数法一
+	(*function_p[1])(10.0, 2.5);						//调用函数法二
+
+	//*(function_p + 1)(10.0,2.5);//错误
+	(*(function_p + 1))(10.0, 2.5);				//调用函数法二的另一种写法
+}
+double function1(double a, double b)
+{}
+double function2 (double a, double b)
+{
+	printf("%.2lf\n", a + b);
+}
+double function3(double a, double b)
+{}
+double function4(double a, double b)
+{}
 #endif
