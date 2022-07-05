ab6f3b547bd5a8419b56da84bcc1e25406d86314
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-07-05 20:03:33 +0800

    Add files via upload

---
+++ C Primer Plus Code practice/chapter 15/code2/Project2/Project2/code2.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//复习题
+//7.a
+struct muban1							//从右往左填充结构位字段，即把第一位字段载入最低位。
+{
+	unsigned int ruanpan : 2;
+	unsigned int : 1;
+	unsigned int CD_ROM : 2;
+	unsigned int : 1;
+	unsigned int yinban : 2;
+};
+//7.b
+struct muban2							//从左往右填充结构位字段，即把第一位字段载入最高位。
+{
+	unsigned int yinban : 2;
+	unsigned int : 1;
+	unsigned int CD_ROM : 2;
+	unsigned int : 1;
+	unsigned int ruanpan : 2;
+};
