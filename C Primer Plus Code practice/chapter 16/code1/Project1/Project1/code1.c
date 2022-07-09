2122dc8d7cc3df22e5614de95edeaccc50e719c2
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-07-09 19:22:20 +0800

    Add files via upload

--- C Primer Plus Code practice/chapter 16/code1/Project1/Project1/code1.c
+++ C Primer Plus Code practice/chapter 16/code1/Project1/Project1/code1.c
@@ -19,14 +19,14 @@ int main(void)
 
 
 //#define预处理指令的用法
-#if 1
+#if 0
 #define TWO 2
 #define FOUR TWO*TWO
 #define DAYIN1 printf("X的值为%d\n",X)
 #define DAYIN2 "X的值是%d\n"
+const char* p = "X的值是%d\n";
 #define MINYAN "consistency is the last refuge of the unimag\
 native"																													//反斜杠+[Enter]把该定义延伸到下一行
-
 
 int main(void)
 {
@@ -34,10 +34,118 @@ int main(void)
 	DAYIN1;
 
 	X = FOUR;
-	printf(DAYIN2,X);
+	printf(DAYIN2,X);			//法一
+	printf(p, X);						//法二
+
 
 	printf("%s\n",MINYAN);
 	printf("TWO:MINYAN\n");
+
+	return 0;
+}
+#endif
+
+
+//使用带参数的宏
+#if 0
+#define SQUARE(X)				X*X
+#define DAYIN(X)					printf("结果是：%d\n",X)
+
+int main(void)
+{
+	int x = 5;
+	int z;
+
+	printf("x=%d\n",x);
+	z = SQUARE(x);
+	printf("SQUARE(x) :");
+	DAYIN(z);
+
+	z = SQUARE(2);
+	printf("SQUARE(2) :");
+	DAYIN(z);
+
+
+	printf("SQUARE(x+2) :");
+	DAYIN(SQUARE(x + 2));				//宏展开后：x+2*x+2=17
+
+	printf("100/SQUARE(2) :");
+	DAYIN(100/SQUARE(2));			//宏展开后：100/2*2=100
+
+	printf("x=%d\n", x);
+	printf("SQUARE(++x) :");				
+	DAYIN(SQUARE(++x));				//宏展开后：++x  *  ++x   求值是未定义行为
+
+	printf("递增后，x=%d\n", x);
+
+	return 0;
+}
+#endif
+
+
+//用#运算符把宏形参替换为实参字符串后进行字符串组合
+#if 0
+#define DAYIN(x)			printf("计算"#x"的平方是：%d\n",((x)*(x)))
+#define DAYIN1(x)			printf("计算#x的平方是：%d\n",((x)*(x)))				//******注******   #x被视为普通文本，而不是个可替换的记号
+
+int main(void)
+{
+	int y = 5;
+
+	DAYIN(y);
+	DAYIN(2+4);
+
+	DAYIN1(y);
+
+	return 0;
+}
+#endif
+
+
+//用##运算符把宏形参替换为实参后进行记号组合
+#if 0
+#define X(n)							x##n
+#define DAYIN_X(n)				printf("x"#n"=%d\n",x##n)
+
+int main(void)
+{
+	int X(1) = 1;							//即int x1=1;
+	int X(2) = 2;
+
+	DAYIN_X(1);						//即printf("x1=%d\n",x1);
+	DAYIN_X(2);
+
+	return 0;
+}
+#endif
+
+
+//使用变参宏
+#if 0
+#include <math.h>
+#define 	DAYIN(X,...)				printf("Message "#X": "__VA_ARGS__)
+
+int main(void)
+{
+	double x = 8;
+	double y;
+
+	y = sqrt(x);
+	DAYIN(1,"x=%g\n",x);
+	DAYIN(2, "y=%.2lf\n", y);
+
+	return 0;
+}
+#endif
+
+
+//使用条件编译
+#if 1
+#define JUST_CHECKING
+#define LIMIT   4
+int main(void)
+{
+
 
 	return 0;
 }
