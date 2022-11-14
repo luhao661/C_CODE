bdc9585e099014ae75594f437a8c23b378a3f3b6
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-11-14 22:17:00 +0800

    Add files via upload

--- Data_Structure/chapter 2/code2/Project2/Project2/3.6.c
+++ Data_Structure/chapter 2/code2/Project2/Project2/3.6.c
@@ -96,6 +96,7 @@ int LocateElem(LinkList L, ElemType e)
 {
     int i = 0;
     LinkList p = L->next;
+
     while (p)
     {
         i++;
@@ -111,17 +112,18 @@ int LocateElem(LinkList L, ElemType e)
 /* 操作结果：在L中第i个位置之前插入新的数据元素e，L的长度加1 */
 Status ListInsert(LinkList* L, int i, ElemType e)
 {
-    int j;
     LinkList temp, s;
+    int j;
 
-    temp = *L;
-    j = 1;
+    temp = *L;//temp一开始指向头结点
+    j = 1;                                //含头节点的链式存储结构
+
     while (temp && j < i)     /* 寻找第i个结点 */
     {
         temp = temp->next;
         ++j;
     }
-    //temp存第i个节点的地址
+    //temp存第i-1个节点的地址
 
     if (!temp || j > i)
         return ERROR;   /* 第i个元素不存在 */
@@ -129,11 +131,13 @@ Status ListInsert(LinkList* L, int i, ElemType e)
     s = (LinkList)malloc(sizeof(Node));  /*  生成新结点(C语言标准函数) */
     s->data = e;
 
-    //新节点的指针域的指针存第i+1个节点的地址
+    //新节点的指针域的指针存原来第i个节点的地址
+    //temp->next存的是原来第i个节点的地址
     s->next = temp->next;      /* 将p的后继结点的地址赋值给s的后继  */
 
-    //???
-    //新节点的地址赋给原本第i+1个节点的指针域的指针
+    //*****************注**********************
+    //新节点的地址赋给原本第i－1个节点的指针域的指针
+    //temp->next现在存的是插入进来的节点的地址
     temp->next = s;          /* 将s赋值给p的后继 */
 
     return OK;
@@ -144,20 +148,26 @@ Status ListInsert(LinkList* L, int i, ElemType e)
 Status ListDelete(LinkList* L, int i, ElemType* e)
 {
     int j;
-    LinkList p, q;
-    p = *L;
+    LinkList temp, q;
+
+    temp = *L;
     j = 1;
-    while (p->next && j < i)	/* 遍历寻找第i个元素 */
+    
+    while (temp->next && j < i)	/* 遍历寻找第i个元素 */
     {
-        p = p->next;
+        temp = temp->next;
         ++j;
     }
-    if (!(p->next) || j > i)
+
+    if (!(temp->next) || j > i)
         return ERROR;           /* 第i个元素不存在 */
-    q = p->next;
-    p->next = q->next;			/* 将q的后继赋值给p的后继 */
-    *e = q->data;               /* 将q结点中的数据给e */
+
+    q = temp->next;
+    temp->next = q->next;			/* 将q的后继赋值给p的后继 */
+    *e = q->data;                        /* 将q结点中的数据给e */
+
     free(q);                    /* 让系统回收此结点，释放内存 */
+
     return OK;
 }
 
@@ -181,13 +191,16 @@ void CreateListHead(LinkList* L, int n)
 {
     LinkList p;
     int i;
+
     srand(time(0));                         /* 初始化随机数种子 */
     *L = (LinkList)malloc(sizeof(Node));
     (*L)->next = NULL;                      /*  先建立一个带头结点的单链表 */
+
     for (i = 0; i < n; i++)
     {
         p = (LinkList)malloc(sizeof(Node)); /*  生成新结点 */
         p->data = rand() % 100 + 1;             /*  随机生成100以内的数字 */
+
         p->next = (*L)->next;
         (*L)->next = p;						/*  插入到表头 */
     }
