1a3fc73015f8ba7ad6dbd8023f368e6cf80b2a37
luhao661 <102971130+luhao661@users.noreply.github.com>
2022-11-24 19:45:33 +0800

    Add files via upload

---
+++ Data_Structure/chapter 6/code3/Project3/Project3/6.10.h
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#ifndef CODE_6_7_2_H
+#define CODE_6_7_2_H
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 100 /* 存储空间初始分配量 */
+
+typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码,如OK等 */
+typedef char TElemType;
+typedef enum { Link, Thread } PointerTag;	/* Link=0表示指向左右孩子指针, */
+										/* Thread=1表示指向前驱或后继的线索 */
+typedef  struct BiThrNode	/* 二叉线索存储结点结构 */
+{
+	TElemType data;	/* 结点数据 */
+	struct BiThrNode* lchild, * rchild;	/* 左右孩子指针 */
+	PointerTag LTag;
+	PointerTag RTag;		/* 左右标志 */
+} BiThrNode, * BiThrTree;
+
+Status CreateBiThrTree(BiThrTree* T);
+void InThreading(BiThrTree p);
+Status InOrderThreading(BiThrTree* Thrt, BiThrTree T);
+Status InOrderTraverse_Thr(BiThrTree T);
+#endif
---
+++ Data_Structure/chapter 6/code3/Project3/Project3/code3.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//线索二叉树
+//链接6.10.c
+#if 1
+#include "6.10.h"
+
+int main()
+{
+	BiThrTree H, T;
+
+	printf("请按前序输入二叉树(如:'ABDH##I##EJ###CF##G##')\n");
+
+	CreateBiThrTree(&T); /* 按前序产生二叉树 */
+	InOrderThreading(&H, T); /* 中序遍历,并中序线索化二叉树 */
+	printf("中序遍历(输出)二叉线索树:\n");
+	InOrderTraverse_Thr(H); /* 中序遍历(输出)二叉线索树 */
+	printf("\n");
+
+	return 0;
+}
+#endif
