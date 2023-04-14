03b31aa9d870528d724cce9626d913f7176571f8
luhao661 <102971130+luhao661@users.noreply.github.com>
2023-04-14 21:50:44 +0800

    Add files via upload

--- CSAPP/chapter 5/Project1/Project1/code1.cpp
+++ CSAPP/chapter 5/Project1/Project1/code1.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 //前缀和与循环展开
-#if 1
+#if 0
 using namespace std;
 
 void psum1(float a [],float p [],long n);
@@ -71,5 +71,112 @@ void psum2(float* a, float* p, long n)
 		p[i] = p[i-1] + a[i];
 
 	return;
+}
+#endif
+
+
+//循环展开
+#if 1
+using namespace std;
+
+typedef long data_t;
+
+/* Create abstract data type for vector */
+typedef struct {
+	long len;
+	data_t * data;
+} vec_rec, * vec_ptr;
+
+/* Create vector of specified length */
+vec_ptr new_vec(long len)
+{
+	/* Allocate header structure */
+	vec_ptr result = (vec_ptr)malloc(sizeof(vec_rec));
+
+	data_t* data = NULL;
+
+	if (!result)
+		return NULL;/* Couldn't allocate storage */
+
+	result->len = len;
+
+	/* Allocate array */
+	if (len > 0) {
+		data = (data_t*)calloc(len, sizeof(data_t));
+		if (!data) {
+			free((void*)result);
+			return NULL;/* Couldn't allocate storage */
+		}
+	}
+	/* Data will either be NULL or allocated array */
+	result->data = data;
+
+	return result;
+}
+/*
+* Retrieve vector element and store at dest.
+* Return 0(out of bounds)or 1(successful)
+*/
+int get_vec_element(vec_ptr v, long index, data_t* dest)
+{
+	 if (index < 0 || index >= v->len)
+		return 0;
+
+	*dest = v->data[index];
+
+	return 1;
+}
+/* Return length of vector */
+long vec_length(vec_ptr v)
+{
+	return v->len;
+}
+
+data_t* get_vec_start(vec_ptr v)
+{
+	return v->data;
+}
+
+//对数组中的元素求和
+#define IDENT 0
+#define OP +
+
+void combine5(vec_ptr v, data_t* dest);
+
+int main()
+{
+	vec_ptr p = new_vec(10);
+
+	p->data[0] = 1;
+	p->data[0] = 2;
+
+	long sum = 0;
+	combine5(p,&sum);
+
+	cout << sum;
+
+	return 0;
+}
+
+//2×1循环展开
+void combine5(vec_ptr v, data_t* dest)
+{
+	long length = vec_length(v);
+	long limit = length - 1;
+
+	data_t* data = get_vec_start(v);
+	data_t acc = IDENT;
+	
+	long i;
+	/* Combine 2 elements at a time */
+	for (i = 0; i < limit; i += 2) {
+		acc = (acc OP data[i])OP data[i + 1];
+	}
+	/* Finish any remaining elements */
+	for (; i < length; i++) {
+		acc = acc OP data[i];
+	}
+
+	*dest = acc;
 }
 #endif
